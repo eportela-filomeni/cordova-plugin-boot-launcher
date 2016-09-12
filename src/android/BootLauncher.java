3c45b46818f633308b32ba89566e65b855d5e77b
eportela-filomeni <eportela@filomeni.com.ar>
2016-09-12 09:48:36 -0300

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,8 +8,10 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, demo.mobile.ionic.MainActivity.class);
-        serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(serviceIntent);
+        startService(new Intent( MainActivity.this,BackgroundService.class));
+        
+        //Intent serviceIntent = new Intent(context, demo.mobile.ionic.MainActivity.class);
+        //serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //context.startActivity(serviceIntent);
     }
 }
