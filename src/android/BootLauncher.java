591ceb51d6caebd6e07c8e69a49cdea3d4cdfbc6
eportela-filomeni <eportela@filomeni.com.ar>
2016-09-12 12:01:09 -0300

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -1,16 +1,20 @@
 package fr.edps.cordova.bootlauncher;
 
-import android.content.Intent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.app.Service;
+import android.content.*;
+
+//import android.content.Intent;
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.app.Service;
+
+import android.app.*;
 import android.os.Binder;
 
 public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent( MainActivity.this,BackgroundService.class));
+        startService(new Intent( MainActivity.this,BackgroundService.class));
         
         //Intent serviceIntent = new Intent(context, demo.mobile.ionic.MainActivity.class);
         //serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
